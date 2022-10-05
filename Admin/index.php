964efb56211b7a38c7a0a8531b816dca9a4bb042
TrHoangLong <90597116+TrHoangLong@users.noreply.github.com>
2022-10-05 11:05:47 +0700

    dashboard

--- Admin/index.php
+++ Admin/index.php
@@ -8,37 +8,57 @@ include('../header.php');
         <div class="row g-4">
             <div class="col-sm-6 col-xl-3">
                 <div class="bg-light rounded d-flex align-items-center justify-content-between p-4">
-                    <i class="fa fa-chart-line fa-3x text-primary"></i>
-                    <div class="ms-3">
-                        <p class="mb-2">Today Sale</p>
-                        <h6 class="mb-0">$1234</h6>
+                    <i class="bi bi-people-fill fa-3x text-primary"></i>
+                    <div class="ms-3" style="text-align: center;">
+                        <p class="mb-2">Nhân viên</p>
+                        <?php
+                        $sql = "Select* from tb_employee Where EmployeeStatus = 1";
+                        $res = mysqli_query($conn, $sql);
+
+                        ?>
+                        <h4 class="mb-0"><?php echo mysqli_num_rows($res); ?></h4>
                     </div>
                 </div>
             </div>
             <div class="col-sm-6 col-xl-3">
                 <div class="bg-light rounded d-flex align-items-center justify-content-between p-4">
-                    <i class="fa fa-chart-bar fa-3x text-primary"></i>
-                    <div class="ms-3">
-                        <p class="mb-2">Total Sale</p>
-                        <h6 class="mb-0">$1234</h6>
+                    <i class="bi bi-laptop fa-3x text-primary"></i>
+                    <div class="ms-3" style="text-align: center;">
+                        <p class="mb-2">Phòng ban</p>
+                        <?php
+                        $sql1 = "Select* from tb_department Where DepartmentStatus = 1";
+                        $res1 = mysqli_query($conn, $sql1);
+
+                        ?>
+                        <h4 class="mb-0"><?php echo mysqli_num_rows($res1); ?></h4>
                     </div>
                 </div>
             </div>
             <div class="col-sm-6 col-xl-3">
                 <div class="bg-light rounded d-flex align-items-center justify-content-between p-4">
-                    <i class="fa fa-chart-area fa-3x text-primary"></i>
-                    <div class="ms-3">
-                        <p class="mb-2">Today Revenue</p>
-                        <h6 class="mb-0">$1234</h6>
+                    <i class="bi bi-calendar-check-fill fa-3x text-primary"></i>
+                    <div class="ms-3" style="text-align: center;">
+                        <p class="mb-2">Chấm công</p>
+                        <?php
+                        $sql2 = "Select* from tb_attendance Where AttendanceStatus = 1";
+                        $res2 = mysqli_query($conn, $sql2);
+
+                        ?>
+                        <h4 class="mb-0"><?php echo mysqli_num_rows($res2); ?></h4>
                     </div>
                 </div>
             </div>
             <div class="col-sm-6 col-xl-3">
                 <div class="bg-light rounded d-flex align-items-center justify-content-between p-4">
-                    <i class="fa fa-chart-pie fa-3x text-primary"></i>
-                    <div class="ms-3">
-                        <p class="mb-2">Total Revenue</p>
-                        <h6 class="mb-0">$1234</h6>
+                    <i class="bi bi-bell-fill fa-3x text-primary" style="text-align: center;"></i>
+                    <div class="ms-3" style="text-align: center;">
+                        <p class="mb-2">Thông báo</p>
+                        <?php
+                        $sql3 = "Select* from tb_notify Where NotifyStatus = 1";
+                        $res3 = mysqli_query($conn, $sql3);
+
+                        ?>
+                        <h4 class="mb-0"><?php echo mysqli_num_rows($res3); ?></h4>
                     </div>
                 </div>
             </div>
@@ -54,7 +74,6 @@ include('../header.php');
                 <div class="bg-light text-center rounded p-4">
                     <div class="d-flex align-items-center justify-content-between mb-4">
                         <h6 class="mb-0">Worldwide Sales</h6>
-                        <a href="">Show All</a>
                     </div>
                     <canvas id="worldwide-sales"></canvas>
                 </div>
@@ -63,7 +82,6 @@ include('../header.php');
                 <div class="bg-light text-center rounded p-4">
                     <div class="d-flex align-items-center justify-content-between mb-4">
                         <h6 class="mb-0">Salse & Revenue</h6>
-                        <a href="">Show All</a>
                     </div>
                     <canvas id="salse-revenue"></canvas>
                 </div>
@@ -74,200 +92,82 @@ include('../header.php');
 
 
     <!-- Recent Sales Start -->
-    <div class="container-fluid pt-4 px-4">
-        <div class="bg-light text-center rounded p-4">
+    <div class="container-fluid pt-4 px-4" style="display: flex;">
+        <div class="bg-light text-center rounded p-4" style="width: 60%; margin-right: 5%;">
             <div class="d-flex align-items-center justify-content-between mb-4">
-                <h6 class="mb-0">Recent Salse</h6>
-                <a href="">Show All</a>
+                <h6 class="mb-0">Thông tin chấm công</h6>
+                <a href="http://localhost/HR_Management/Admin/Attendance">Show All</a>
             </div>
             <div class="table-responsive">
                 <table class="table text-start align-middle table-bordered table-hover mb-0">
                     <thead>
                         <tr class="text-dark">
-                            <th scope="col"><input class="form-check-input" type="checkbox"></th>
-                            <th scope="col">Date</th>
-                            <th scope="col">Invoice</th>
-                            <th scope="col">Customer</th>
-                            <th scope="col">Amount</th>
-                            <th scope="col">Status</th>
-                            <th scope="col">Action</th>
+                            <th scope="col">Ngày</th>
+                            <th scope="col">Nhân viên</th>
+                            <th scope="col">Phòng ban</th>
+                            <th scope="col">Tình trạng</th>
+                            <th scope="col">Số ngày đã chấm công</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td><input class="form-check-input" type="checkbox"></td>
-                            <td>01 Jan 2045</td>
-                            <td>INV-0123</td>
-                            <td>Jhon Doe</td>
-                            <td>$123</td>
-                            <td>Paid</td>
-                            <td><a class="btn btn-sm btn-primary" href="">Detail</a></td>
-                        </tr>
-                        <tr>
-                            <td><input class="form-check-input" type="checkbox"></td>
-                            <td>01 Jan 2045</td>
-                            <td>INV-0123</td>
-                            <td>Jhon Doe</td>
-                            <td>$123</td>
-                            <td>Paid</td>
-                            <td><a class="btn btn-sm btn-primary" href="">Detail</a></td>
-                        </tr>
-                        <tr>
-                            <td><input class="form-check-input" type="checkbox"></td>
-                            <td>01 Jan 2045</td>
-                            <td>INV-0123</td>
-                            <td>Jhon Doe</td>
-                            <td>$123</td>
-                            <td>Paid</td>
-                            <td><a class="btn btn-sm btn-primary" href="">Detail</a></td>
-                        </tr>
-                        <tr>
-                            <td><input class="form-check-input" type="checkbox"></td>
-                            <td>01 Jan 2045</td>
-                            <td>INV-0123</td>
-                            <td>Jhon Doe</td>
-                            <td>$123</td>
-                            <td>Paid</td>
-                            <td><a class="btn btn-sm btn-primary" href="">Detail</a></td>
-                        </tr>
-                        <tr>
-                            <td><input class="form-check-input" type="checkbox"></td>
-                            <td>01 Jan 2045</td>
-                            <td>INV-0123</td>
-                            <td>Jhon Doe</td>
-                            <td>$123</td>
-                            <td>Paid</td>
-                            <td><a class="btn btn-sm btn-primary" href="">Detail</a></td>
-                        </tr>
+
+                        <?php
+                        $sql4 = "SELECT emp.EmployeeCode, emp.EmployeeName, dep.DepartmentName, att.AttendanceStart, COUNT(*) AS NumberAttendance 
+                                FROM tb_employee emp, tb_department dep, tb_attendance att 
+                                WHERE emp.DepartmentCode = dep.DepartmentCode 
+                                AND emp.EmployeeCode = att.EmployeeCode 
+                                AND att.AttendanceStatus = 1
+                                GROUP BY emp.EmployeeCode
+                                ";
+                        $res4 = mysqli_query($conn, $sql4);
+
+                        while ($row4 = mysqli_fetch_assoc($res4)) {
+                            $empName = $row4['EmployeeName'];
+                            $depName = $row4['DepartmentName'];
+                            $attStart = $row4['AttendanceStart'];
+                            $numberAtt = $row4['NumberAttendance'];
+
+                            $date = new DateTime($attStart);
+                            $newDate = $date -> format('d/m/y');
+                        ?>
+                            <tr>
+                                <td><?php echo date("d/m/y") ?></td>
+                                <td><?php echo $empName; ?></td>
+                                <td><?php echo $depName; ?></td>
+                                <td>
+                                    <?php
+                                        if(date("d/m/y") == $newDate) {
+                                            echo 'Đã chấm công';
+                                        } else {
+                                            echo 'Chưa chấm công';
+                                        }
+                                    ?>
+                                </td>
+                                <td><?php echo $numberAtt; ?></td>
+                            </tr>
+                        <?php
+                        }
+                        ?>
+
+
                     </tbody>
                 </table>
             </div>
         </div>
-    </div>
-    <!-- Recent Sales End -->
-
-
-    <!-- Widgets Start -->
-    <div class="container-fluid pt-4 px-4">
         <div class="row g-4">
-            <div class="col-sm-12 col-md-6 col-xl-4">
-                <div class="h-100 bg-light rounded p-4">
-                    <div class="d-flex align-items-center justify-content-between mb-2">
-                        <h6 class="mb-0">Messages</h6>
-                        <a href="">Show All</a>
-                    </div>
-                    <div class="d-flex align-items-center border-bottom py-3">
-                        <img class="rounded-circle flex-shrink-0" src="img/user.jpg" alt="" style="width: 40px; height: 40px;">
-                        <div class="w-100 ms-3">
-                            <div class="d-flex w-100 justify-content-between">
-                                <h6 class="mb-0">Jhon Doe</h6>
-                                <small>15 minutes ago</small>
-                            </div>
-                            <span>Short message goes here...</span>
-                        </div>
-                    </div>
-                    <div class="d-flex align-items-center border-bottom py-3">
-                        <img class="rounded-circle flex-shrink-0" src="img/user.jpg" alt="" style="width: 40px; height: 40px;">
-                        <div class="w-100 ms-3">
-                            <div class="d-flex w-100 justify-content-between">
-                                <h6 class="mb-0">Jhon Doe</h6>
-                                <small>15 minutes ago</small>
-                            </div>
-                            <span>Short message goes here...</span>
-                        </div>
-                    </div>
-                    <div class="d-flex align-items-center border-bottom py-3">
-                        <img class="rounded-circle flex-shrink-0" src="img/user.jpg" alt="" style="width: 40px; height: 40px;">
-                        <div class="w-100 ms-3">
-                            <div class="d-flex w-100 justify-content-between">
-                                <h6 class="mb-0">Jhon Doe</h6>
-                                <small>15 minutes ago</small>
-                            </div>
-                            <span>Short message goes here...</span>
-                        </div>
-                    </div>
-                    <div class="d-flex align-items-center pt-3">
-                        <img class="rounded-circle flex-shrink-0" src="img/user.jpg" alt="" style="width: 40px; height: 40px;">
-                        <div class="w-100 ms-3">
-                            <div class="d-flex w-100 justify-content-between">
-                                <h6 class="mb-0">Jhon Doe</h6>
-                                <small>15 minutes ago</small>
-                            </div>
-                            <span>Short message goes here...</span>
-                        </div>
-                    </div>
-                </div>
+
+        </div>
+        <div class="h-100 bg-light rounded p-4" style="width: 35%;">
+            <div class="d-flex align-items-center justify-content-between mb-4">
+                <h6 class="mb-0">Calender</h6>
             </div>
-            <div class="col-sm-12 col-md-6 col-xl-4">
-                <div class="h-100 bg-light rounded p-4">
-                    <div class="d-flex align-items-center justify-content-between mb-4">
-                        <h6 class="mb-0">Calender</h6>
-                        <a href="">Show All</a>
-                    </div>
-                    <div id="calender"></div>
-                </div>
-            </div>
-            <div class="col-sm-12 col-md-6 col-xl-4">
-                <div class="h-100 bg-light rounded p-4">
-                    <div class="d-flex align-items-center justify-content-between mb-4">
-                        <h6 class="mb-0">To Do List</h6>
-                        <a href="">Show All</a>
-                    </div>
-                    <div class="d-flex mb-2">
-                        <input class="form-control bg-transparent" type="text" placeholder="Enter task">
-                        <button type="button" class="btn btn-primary ms-2">Add</button>
-                    </div>
-                    <div class="d-flex align-items-center border-bottom py-2">
-                        <input class="form-check-input m-0" type="checkbox">
-                        <div class="w-100 ms-3">
-                            <div class="d-flex w-100 align-items-center justify-content-between">
-                                <span>Short task goes here...</span>
-                                <button class="btn btn-sm"><i class="fa fa-times"></i></button>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="d-flex align-items-center border-bottom py-2">
-                        <input class="form-check-input m-0" type="checkbox">
-                        <div class="w-100 ms-3">
-                            <div class="d-flex w-100 align-items-center justify-content-between">
-                                <span>Short task goes here...</span>
-                                <button class="btn btn-sm"><i class="fa fa-times"></i></button>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="d-flex align-items-center border-bottom py-2">
-                        <input class="form-check-input m-0" type="checkbox" checked>
-                        <div class="w-100 ms-3">
-                            <div class="d-flex w-100 align-items-center justify-content-between">
-                                <span><del>Short task goes here...</del></span>
-                                <button class="btn btn-sm text-primary"><i class="fa fa-times"></i></button>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="d-flex align-items-center border-bottom py-2">
-                        <input class="form-check-input m-0" type="checkbox">
-                        <div class="w-100 ms-3">
-                            <div class="d-flex w-100 align-items-center justify-content-between">
-                                <span>Short task goes here...</span>
-                                <button class="btn btn-sm"><i class="fa fa-times"></i></button>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="d-flex align-items-center pt-2">
-                        <input class="form-check-input m-0" type="checkbox">
-                        <div class="w-100 ms-3">
-                            <div class="d-flex w-100 align-items-center justify-content-between">
-                                <span>Short task goes here...</span>
-                                <button class="btn btn-sm"><i class="fa fa-times"></i></button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <div id="calender"></div>
         </div>
     </div>
-    <!-- Widgets End -->
-    
+</div>
+<!-- Recent Sales End -->
+
+
 </div>
 <?php
 include('../footer.php');
