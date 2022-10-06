41ef9e0bf610fff4c47989ceeb8e752b68a6f4d4
hohongquan25 <hohongquan25@>
2022-10-06 20:12:37 +0700

    quan vip

--- Emloyee/salary.php
+++ Emloyee/salary.php
@@ -1,8 +1,7 @@
 <?php
-    include('../../ConnectDatabase/connect.php');
-    include('../header.php');  
-    session_start();
-    $employeecode1 = $_SESSION['id_emloyeeSession'];
+
+    include('header.php');  
+  
 ?>
 
 <?php
