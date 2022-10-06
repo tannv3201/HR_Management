0554b7c2c1d0ce42efa4b73392aac0c802234a51
minhvuong25 <90454436+minhvuong25@users.noreply.github.com>
2022-10-06 20:38:54 +0700

    notifi

--- Emloyee/header.php
+++ Emloyee/header.php
@@ -1,16 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
-<?php 
-        session_start();
-        $employeecode1= $_SESSION['id_emloyeeSession'];
-        include('connect.php');
-        if(!isset($_SESSION['id_emloyeeSession'])) //nếu chưa đăng nhập thì ra ngoài
-        {
-            header("Location:../index.php");
-        }
-        ob_start();
-        
+<?php
+include('../ConnectDatabase/connect.php');
+session_start();
+$employeecode1 = $_SESSION['id_emloyeeSession'];
+ob_start();
+
 ?>
+
 <head>
     <meta charset="utf-8">
     <title>HR_Management</title>
@@ -21,9 +18,7 @@
 
     <!-- Favicon -->
     <link href="img/favicon.ico" rel="icon">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
-        integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- Google Web Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -42,14 +37,13 @@
 
     <!-- Template Stylesheet -->
     <link href="http://localhost/HR_Management/css/style.css" rel="stylesheet">
-    
+
 </head>
 
 <body>
     <div class="container-fluid position-relative bg-white d-flex p-0">
         <!-- Spinner Start -->
-        <div id="spinner"
-            class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+        <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
             <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
                 <span class="sr-only">Loading...</span>
             </div>
@@ -65,35 +59,23 @@
                 </a>
                 <div class="d-flex align-items-center ms-4 mb-4">
                     <div class="position-relative">
-                        <img class="rounded-circle" src="http://localhost/HR_Management/img/user.jpg" alt=""
-                            style="width: 40px; height: 40px;">
-                        <div
-                            class="bg-success rounded-circle border border-2 border-white position-absolute end-0 bottom-0 p-1">
+                        <img class="rounded-circle" src="http://localhost/HR_Management/img/user.jpg" alt="" style="width: 40px; height: 40px;">
+                        <div class="bg-success rounded-circle border border-2 border-white position-absolute end-0 bottom-0 p-1">
                         </div>
                     </div>
                     <div class="ms-3">
-                        <?php
-                            $sql = "SELECT * FROM `tb_employee` WHERE EmployeeCode = '$employeecode1'";
-                            $res = mysqli_query($conn,$sql);
-                            $row = mysqli_fetch_assoc($res);
-                        ?>
                         <h6 class="mb-0"></h6>
-                        <h5><?php echo $row['EmployeeName'] ?></h5>
+                        <span>Admin</span>
                     </div>
                 </div>
                 <div class="navbar-nav w-100">
-                    <a href="http://localhost/HR_Management/Emloyee" class="nav-item nav-link active"><i
-                            class="fa fa-tachometer-alt me-2"></i>Trang chủ</a>
+                    <a href="http://localhost/HR_Management/Emloyee" class="nav-item nav-link active"><i class="fa fa-tachometer-alt me-2"></i>Trang chủ</a>
                     <div class="nav-item dropdown">
-                        <a href="http://localhost/HR_Management/Emloyee/information.php" class="nav-link"><i
-                                class="fa fa-user me-2"></i>Thông tin cá nhân</a>
+                        <a href="http://localhost/HR_Management/Emloyee/information.php" class="nav-link"><i class="fa fa-user me-2"></i>Thông tin cá nhân</a>
                     </div>
-                    <a href="http://localhost/HR_Management/Emloyee/attendance.php" class="nav-link"><i
-                            class="fa fa-calendar-days me-2"></i>Chấm công</a>
-                    <a href="http://localhost/HR_Management/Emloyee/salary.php" class="nav-item nav-link"><i
-                            class="fa fa-dollar me-2"></i>Lương</a>
-                    <a href="http://localhost/HR_Management/Emloyee/notify.php" class="nav-item nav-link"><i
-                            class="fa fa-bell me-lg-2"></i>Thông báo</a>
+                    <a href="http://localhost/HR_Management/Emloyee/attendance.php" class="nav-link"><i class="fa fa-calendar-days me-2"></i>Chấm công</a>
+                    <a href="http://localhost/HR_Management/Emloyee/salary.php" class="nav-item nav-link"><i class="fa fa-dollar me-2"></i>Lương</a>
+                    <a href="http://localhost/HR_Management/Emloyee/notify.php" class="nav-item nav-link"><i class="fa fa-bell me-lg-2"></i>Thông báo</a>
                 </div>
             </nav>
         </div>
@@ -116,8 +98,7 @@
                         <div class="dropdown-menu dropdown-menu-end bg-light border-0 rounded-0 rounded-bottom m-0">
                             <a href="#" class="dropdown-item">
                                 <div class="d-flex align-items-center">
-                                    <img class="rounded-circle" src="img/user.jpg" alt=""
-                                        style="width: 40px; height: 40px;">
+                                    <img class="rounded-circle" src="img/user.jpg" alt="" style="width: 40px; height: 40px;">
                                     <div class="ms-2">
                                         <h6 class="fw-normal mb-0">Jhon send you a message</h6>
                                         <small>15 minutes ago</small>
@@ -127,8 +108,7 @@
                             <hr class="dropdown-divider">
                             <a href="#" class="dropdown-item">
                                 <div class="d-flex align-items-center">
-                                    <img class="rounded-circle" src="img/user.jpg" alt=""
-                                        style="width: 40px; height: 40px;">
+                                    <img class="rounded-circle" src="img/user.jpg" alt="" style="width: 40px; height: 40px;">
                                     <div class="ms-2">
                                         <h6 class="fw-normal mb-0">Jhon send you a message</h6>
                                         <small>15 minutes ago</small>
@@ -138,8 +118,7 @@
                             <hr class="dropdown-divider">
                             <a href="#" class="dropdown-item">
                                 <div class="d-flex align-items-center">
-                                    <img class="rounded-circle" src="img/user.jpg" alt=""
-                                        style="width: 40px; height: 40px;">
+                                    <img class="rounded-circle" src="img/user.jpg" alt="" style="width: 40px; height: 40px;">
                                     <div class="ms-2">
                                         <h6 class="fw-normal mb-0">Jhon send you a message</h6>
                                         <small>15 minutes ago</small>
@@ -156,7 +135,7 @@
                             <span class="d-none d-lg-inline-flex">Notificatin</span>
                         </a>
                         <div class="dropdown-menu dropdown-menu-end bg-light border-0 rounded-0 rounded-bottom m-0">
-                            <a href="#" class="dropdown-item">
+                            <!-- <a href="#" class="dropdown-item">
                                 <h6 class="fw-normal mb-0">Profile updated</h6>
                                 <small>15 minutes ago</small>
                             </a>
@@ -164,21 +143,29 @@
                             <a href="#" class="dropdown-item">
                                 <h6 class="fw-normal mb-0">New user added</h6>
                                 <small>15 minutes ago</small>
-                            </a>
+                            </a> -->
+                            <?php
+                            $sql = "SELECT * FROM `tb_notify` limit 3";
+                            $qr = mysqli_query($conn, $sql);
+                            while ($row = mysqli_fetch_assoc($qr)) {
+                            ?>
+                                <hr class="dropdown-divider">
+                                <a href="notify.php?id=<?=$row['Id']?>" class="dropdown-item">
+                                    <h6 class="fw-normal mb-0"><?=$row['NotifyName']?></h6>
+                                    <small><?=$row['NotifyContent']?></small>
+                                </a>
+                            <?php
+                            }
+                            ?>
+
                             <hr class="dropdown-divider">
-                            <a href="#" class="dropdown-item">
-                                <h6 class="fw-normal mb-0">Password changed</h6>
-                                <small>15 minutes ago</small>
-                            </a>
-                            <hr class="dropdown-divider">
-                            <a href="#" class="dropdown-item text-center">See all notifications</a>
+                            <a href="notify.php" class="dropdown-item text-center">See all notifications</a>
                         </div>
                     </div>
                     <div class="nav-item dropdown">
                         <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
-                            <img class="rounded-circle me-lg-2" src="http://localhost/HR_Management/img/user.jpg" alt=""
-                                style="width: 40px; height: 40px;">
-                            <span class="d-none d-lg-inline-flex"><?php echo $row['EmployeeName'] ?></span>
+                            <img class="rounded-circle me-lg-2" src="http://localhost/HR_Management/img/user.jpg" alt="" style="width: 40px; height: 40px;">
+                            <span class="d-none d-lg-inline-flex">Trịnh Long</span>
                         </a>
                         <div class="dropdown-menu dropdown-menu-end bg-light border-0 rounded-0 rounded-bottom m-0">
                             <a href="#" class="dropdown-item">Log Out</a>
